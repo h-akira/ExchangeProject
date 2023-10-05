023ddb0f5bcff04652a2338ed1ed5155b7b037f2
h-akira <79441679+h-akira@users.noreply.github.com>
2023-10-06 03:49:11 +0900

    update

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+class IndexView(generic.TemplateView):
+  template_name = 'home/index.html'
