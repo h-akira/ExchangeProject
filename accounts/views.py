f71282c81deabc8cf062fbbd58ca20e1f9c22f95
h-akira <79441679+h-akira@users.noreply.github.com>
2023-10-06 03:26:22 +0900

    update

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+# from .models import CustomUser
 
-# Create your views here.
+class SignUpView(generic.CreateView):
+  form_class = UserCreationForm
+  success_url = reverse_lazy('login')
+  template_name = 'accounts/signup.html'
+
+class CannotSignUpView(generic.TemplateView):
+  template_name = 'accounts/cannot_signup.html'
+
