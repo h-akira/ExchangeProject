40a1cad3df617f5fd57cc266809389c11dd44fe5
h-akira <79441679+h-akira@users.noreply.github.com>
2023-10-08 02:03:33 +0900

    update

--- bin/json2django.py
+++ bin/json2django.py
@@ -6,10 +6,8 @@
 import sys
 import os
 import glob
-import re
+import json
 import datetime
-from pytz import timezone
-import pandas as pd
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 def parse_args():
@@ -18,105 +16,48 @@ def parse_args():
 データベースに経済カレンダーの情報を追加する．
 """, formatter_class = argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument("--version", action="version", version='%(prog)s 0.0.1')
-  # parser.add_argument("-s", "--start", metavar="日付", help="2023-10-06などの形式の日付（Noneなら現在の7日前）")
-  # parser.add_argument("-p", "--pairs", metavar="pair", nargs="*", default=["USDJPY","EURJPY","EURUSD","GBPJPY", "AUDJPY"], help="通貨ペア")
+  parser.add_argument("--encoding", metavar="encoding", default="utf-8", help="encoding")
+  parser.add_argument("-u", "--username", metavar="username", help="username")
   # parser.add_argument("-", "--", action="store_true", help="")
-  # parser.add_argument("file", metavar="input-file", help="input file")
+  parser.add_argument("file", metavar="input-file", help="json file")
   options = parser.parse_args()
   return options
 
-def GMO_dir2DataFrame(dir_name,pair="USDJPY",date_range=None, BID_ASK="BID"):
-  # pairは"/"を含んでいてもいなくても処理可能
-  # date_rangeはdatetime.date型を要素に持つリストまたはタプル
-  # ディレクトリ構造は以下の通り:
-  # .
-  # |-USDJPY
-  # | |-202303
-  # | | |-USDJPY_20230301.csv
-  # | | |-USDJPY_20230302.csv
-  # | | |-USDJPY_20230303.csv
-  # | | |-...
-  # | |-202304
-  # | | |-...
-  # | |-202305
-  # |   |-...
-  # |-EURJPY
-  #    |-...
-  file_list = glob.glob(os.path.join(dir_name,pair.replace("/",""))+f"/*/{pair.replace('/','')}_*.csv")
-  df = pd.DataFrame()
-  for file in file_list:
-    if os.path.basename(file)[:len(pair.replace("/",""))] != pair.replace("/","") or file[-4:] != ".csv":
-      print(f"skip: {file}")
-      continue
-    m = re.search(r"\d{4}\d{2}\d{2}", file)
-    if m:
-      file_date = datetime.datetime.strptime(m.group(),"%Y%m%d").date()  # 日付文字列を取得
-    else:
-      continue
-    if date_range != None:
-      if date_range[0] <= file_date < date_range[1]:
-        pass
-      else:
-        continue
-    df = pd.concat([df,GMO_csv2DataFrame(file,BID_ASK=BID_ASK)])
-  df = df.sort_values(by="date", ascending=True)
-  return df
-
-def GMO_csv2DataFrame(file_name,BID_ASK="BID"):
-  # GMOクリック証券からダウンロードしたヒストリカルデータ（CSVファイル）を読み込み，
-  # mplfinanceで扱えるデータフレームにして返す．
-  if not os.path.isfile(file_name):
-    raise FileNotFoundError(f"{file_name}は存在しません．")
-  df = pd.read_csv(file_name, encoding='shift_jis').rename(
-    columns={
-      '日時':'date', 
-      f'始値({BID_ASK})':'Open', 
-      f'高値({BID_ASK})':'High', 
-      f'安値({BID_ASK})':'Low', 
-      f'終値({BID_ASK})':'Close'
-    }
-  )
-  for col in df:
-    if col not in ["Open", "High", "Low", "Close", "date"]:
-      df = df.drop(col, axis=1) 
-  df["date"] = pd.to_datetime(df["date"])
-  df.set_index("date", inplace=True)
-  df.index = df.index.tz_localize(timezone('Asia/Tokyo'))
-  return df
-
 def main():
   options = parse_args()
+  if not opsions.username:
+    if "y" != input("usernameが指定されていませんが構いませんか？(y/other):")
+      sys.exit()
   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ExchangeProject.settings")
   import django
   django.setup()
   from django.conf import settings
-  from api.models import ExchangeDataTable
-  now = datetime.datetime.now()-datetime.timedelta(hours=6)
-  if options.start == None:
-    options.start = now - datetime.timedelta(days=7)
-  else:
-    options.start = datetime.datetime.strptime(options.start, "%Y-%m-%d")
-  date_range = (options.start.date(), (now + datetime.timedelta(days=1)).date())
-  for pair in options.pairs:
-    df = GMO_dir2DataFrame(
-      os.path.join(settings.BASE_DIR, "data/rate"), 
-      pair=pair,
-      date_range=date_range
-    ) 
-    dict_list = [{'dt': index, **row.to_dict()} for index, row in df.iterrows()]
-    obj_list = []
-    for d in dict_list:
-      d["pair"]=pair
-      obj = ExchangeDataTable(**d)
-      obj_list.append(obj)
-      # try:
-      #   obj.save()
-      #   if d["dt"].hour == 7 and d["dt"].minute == 0:
-      #     print(f"{d['dt'].date()}の{d['pair']}を保存しました．")
-      # except django.db.utils.IntegrityError:
-      #   if d["dt"].hour == 7 and d["dt"].minute == 0:
-      #     print(f"{d['dt'].date()}の{d['pair']}は既に存在します．")
-    ExchangeDataTable.objects.bulk_create(obj_list, ignore_conflicts=True)
+  from diary.models import EventTable
+  from accounts.models import CustomUser
+  user = CustomUser.objects.get(username=options.username)
+  data = json.load(open(options.file, mode="r", encoding=options.encoding))
+  print(data)
+  for d in data:
+    if d["importance"] < 2:
+      continue
+    date = datetime.datetime.strptime(d["date"], "%Y-%m-%d").date()
+    if d["time"] != "":
+      time = datetime.datetime.strptime(d["time"], "%H:%M").time()
+    else:
+      time = None
+    EventTable.objects.update_or_create(
+      user = user,
+      name = d["name"],
+      country = d["country"],
+      date = date,
+      time = time,
+      defaults = {
+        "importance" : d["importance"],
+        "previous" : d["previous"],
+        "prediction" : d["prediction"],
+        "result" : d["result"]
+      }
+    )
 
 if __name__ == '__main__':
   main()
