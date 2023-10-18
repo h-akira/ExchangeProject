76af61fc8961dff6ec1ee5335a38eae9c62139b5
h-akira <79441679+h-akira@users.noreply.github.com>
2023-10-18 16:21:44 +0900

    success

--- ExchangeProject/settings.py
+++ ExchangeProject/settings.py
@@ -110,7 +110,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
+FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "home:index"
