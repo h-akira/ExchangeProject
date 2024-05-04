c75a05a78f279a671924c077ac4f6b927540fb7f
h-akira <79441679+h-akira@users.noreply.github.com>
2024-05-05 03:39:04 +0900

    update

--- chart/urls.py
+++ chart/urls.py
@@ -5,7 +5,10 @@ app_name = "chart"
 
 urlpatterns = [
   path('', views.index, name='index'),
-  path('<int:category_number>/', views.index, name='category'),
+  path('<int:category_number>', views.index, name='category'),
+  path('category/update/<int:pk>', views.CategoryUpdateView.as_view(), name='category_update'),
+  path('category/create/', views.CategoryCreateView.as_view(), name='category_create'),
+  path('category/delete/<int:pk>', views.CategoryDeleteView.as_view(), name='category_delete'),
   # path('detail/<str:date>/', views.detail, name='detail'),
   # path('detail/<str:date>/<str:option>/', views.detail, name='detail'),
   # path('update/<str:date>/', views.update, name='update'),
--- chart/views.py
+++ chart/views.py
@@ -1,20 +1,82 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from .models import ChartTable, CategoryTable,LinkTable
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 @login_required
-def index(request, category_number=0):
-  categorys = CategoryTable.objects.filter(user=request.user)
-  if category_number != 0:
-    if category_number <= categorys.count()-1:
-      redirect('chart:index')
-  # now_open = []
-  if categorys.exists():
-    for i, category in enumerate(categorys):
+def index(request, category_number=1):
+  categories = CategoryTable.objects.filter(user=request.user)
+  if category_number != 1:
+    if category_number > categories.count():
+      return redirect('chart:index')
+  if categories.exists():
+    for i, category in enumerate(categories):
       if i == category_number:
         charts = ChartTable.objects.filter(category=category)
   context = {
-    'categorys': categorys,
+    'categories': categories,
     'category_number': category_number,
   }
-  return render(request, 'chart/index.html')
+  return render(request, 'chart/index.html', context)
+
+# ここからCategory
+class CategoryCreateView(LoginRequiredMixin, CreateView):
+  model = CategoryTable
+  fields = ('name','description')
+  template_name = 'chart/category_edit.html'
+  success_url = reverse_lazy('chart:index')
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super(CategoryCreateView, self).form_valid(form)
+  def get_context_data(self):
+    context = super().get_context_data()
+    context["edit_type"] = "create"
+    return context
+
+class CategoryUpdateView(LoginRequiredMixin, UpdateView):
+  model = CategoryTable
+  fields = ('name','description')
+  template_name = 'chart/category_edit.html'
+  success_url = reverse_lazy('chart:index')
+  def get_context_data(self):
+    context = super().get_context_data()
+    context["edit_type"] = "update"
+    return context
+
+class CategoryDeleteView(LoginRequiredMixin, DeleteView):
+  model = CategoryTable
+  template_name = 'chart/category_delete.html'
+  success_url = reverse_lazy('chart:index')
+
+# ここからchart
+# class ChartCreateView(LoginRequiredMixin, CreateView):
+#   model = CategoryTable
+#   fields = ('name','discription')
+#   template_name = 'chart/category_edit.html'
+#   success_url = reverse_lazy('chart:index')
+#   def form_valid(self, form):
+#     form.instance.user = self.request.user
+#     return super(CategoryCreateView, self).form_valid(form)
+#   def get_context_data(self):
+#     context = super().get_context_data()
+#     context["edit_type"] = "create"
+#     return context
+#
+# class CategoryUpdateView(LoginRequiredMixin, UpdateView):
+#   model = CategoryTable
+#   fields = ('name','discription')
+#   template_name = 'chart/category_edit.html'
+#   success_url = reverse_lazy('chart:index')
+#   def get_context_data(self):
+#     context = super().get_context_data()
+#     context["edit_type"] = "update"
+#     return context
+#
+# class CategoryDeleteView(LoginRequiredMixin, DeleteView):
+#   model = CategoryTable
+#   template_name = 'chart/category_delete.html'
+#   success_url = reverse_lazy('chart:index')
+#
+#
